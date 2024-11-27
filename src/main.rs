8d708298a8a375fa958fc70dc84a9d8b5c11972e
PonponJuice <ponjuice2021@gmail.com>
2024-11-28 01:37:03 +0900

    とりま

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,56 @@ use std::{env, net::SocketAddr};
 use axum::{body::Bytes, extract::State, routing::post, Router};
 use http::{HeaderMap, StatusCode};
 use tokio::net::TcpListener;
+use traq::apis::configuration::Configuration;
 use traq_bot_http::{Event, RequestParser};
+
+#[derive(Clone)]
+struct App {
+    request_parser: RequestParser,
+    client_config: Configuration,
+}
 
 #[tokio::main]
 async fn main() {
     let verification_token = env::var("VERIFICATION_TOKEN").unwrap();
-    let bot_access_token = env::var("BOT_ACCESS_TOKEN").unwrap();
-    let port = env::var("PORT").unwrap().parse().unwrap();
-
-    let parser = RequestParser::new(&verification_token);
-    let app = Router::new().route("/", post(handler)).with_state(parser);
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    let listener = TcpListener::bind(&addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let access_token = env::var("BOT_ACCESS_TOKEN").unwrap();
+    let request_parser = RequestParser::new(&verification_token);
+    let client_config = Configuration {
+        bearer_access_token: Some(access_token),
+        ..Default::default()
+    };
+    let app = App {
+        request_parser,
+        client_config,
+    };
+    let router = Router::new().route("/", post(handler)).with_state(app);
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let server = TcpListener::bind(addr).await.unwrap();
+    axum::serve(server, router).await.unwrap();
 }
 
-async fn handler(
-    State(parser): State<RequestParser>,
-    headers: HeaderMap,
-    body: Bytes,
-) -> StatusCode {
-    match parser.parse(headers.iter(), &body) {
+async fn handler(State(app): State<App>, headers: HeaderMap, body: Bytes) -> StatusCode {
+    match app.request_parser.parse(headers.iter(), &body) {
         Ok(Event::MessageCreated(payload)) => {
             print!(
                 "{}さんがメッセージを投稿しました。\n内容: {}\n",
                 payload.message.user.display_name, payload.message.text
             );
+            StatusCode::NO_CONTENT
+        }
+        Ok(Event::DirectMessageCreated(payload)) => {
+            use traq::apis::message_api::post_direct_message;
+            let user = payload.message.user;
+            println!("{}さんにDMで返答", user.display_name);
+            let request = traq::models::PostMessageRequest {
+                content: ":oisu-:".to_string(),
+                embed: None,
+            };
+            let res = post_direct_message(&app.client_config, &user.id, Some(request)).await;
+            if let Err(e) = res {
+                eprintln!("{e}");
+                return StatusCode::INTERNAL_SERVER_ERROR;
+            }
             StatusCode::NO_CONTENT
         }
         Ok(_) => StatusCode::NO_CONTENT,
